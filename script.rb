adb624b24921bc84e3f33c8a38f149443b999a95
Andrew Kim <andrew@Andrews-MacBook-Pro.local>
2021-09-21 17:48:47 -0500

    add basic error handling

--- script.rb
+++ script.rb
@@ -4,26 +4,34 @@ require 'pry'
 
 export_workspace = Intercom::Client.new(token: #ACCESS_TOKEN)
 import_workspace = Intercom::Client.new(token: #ACCESS_TOKEN)
-imported_collection_name = #NAME_OF_COLLECTION
+imported_collection_name = #COLLECTION_NAME
 
 collection = import_workspace.collections.create(name: imported_collection_name)
 collection_id = collection.id
-author_id = #ADMIN_ID
-
+author_id = #AUTHOR_ID
+failed_articles = []
 exported_articles = export_workspace.articles.all
 
 exported_articles.each do |export_article|
+    
     if export_article.state == "published"
-       imported_article = import_workspace.articles.create({
-            title: export_article.title, 
-            body: export_article.body,
-            parent_type: "collection", 
-            parent_id: collection.id,
-            state: "draft",
+        begin 
+            imported_article = import_workspace.articles.create({
+                title: export_article.title, 
+                body: export_article.body,
+                parent_type: "collection", 
+                parent_id: collection.id,
+                state: "draft",
             author_id: author_id
-        })
-        print "\n Copying Article #{imported_article.title}"
+            })
+            print "\n Copying Article #{imported_article.title}"
+        rescue
+            print "\nFailed to import article...moving on"
+            failed_articles << export_article.title
+         end
     end
-    "All done! Check out your Articles in Intercom!"
 end
+
+print "\n All done! Check out your Articles in Intercom!"
+print "\n Here are the titles of the Article(s) that I couldn't copy: #{failed_articles}. Please check if there's any wonky formatting in said Article(s)"
 
