379626464d46a172b1e191b18151394ab71ac9d2
Andrew Kim <andrew@Andrews-MacBook-Pro.local>
2021-10-13 11:55:11 -0500

    init readability

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
-EXPORT_ACCESS_TOKEN = "dG9rOjhlZWE4ZTRmXzRjYzlfNGVhY19iMjQ2X2U2NTIxNmI4OTU5NjoxOjA="
-IMPORT_ACCESS_TOKEN = "dG9rOjA1ZWE4MDcxXzhiMmVfNGVlMF9hMjFlXzQ5ODY2ODY2YzJhYToxOjA="
-COLLECTION_NAME = "last one?????"
-AUTHOR_ID = 708351
+EXPORT_ACCESS_TOKEN = “YOUR ACCESS TOKEN“
+IMPORT_ACCESS_TOKEN = “YOUR ACCESS TOKEN“
+COLLECTION_NAME = "YOUR COLLECTION NAME"
+AUTHOR_ID = "YOUR ADMIN ID"
 
 
 #If you need to change URLS within your Articles that point to your old Helpcenter, fill in these fields. Otherwise, leave blank.
 REPLACE_ARTICLE_URLS = true
-EXPORT_CUSTOM_DOMAIN = "docs.intercom.com"
-IMPORT_CUSTOM_DOMAIN = "crocks.intercom.com"
+EXPORT_CUSTOM_DOMAIN = "YOUR CUSTOM DOMAIN HERE"
+IMPORT_CUSTOM_DOMAIN = "NEW CUSTOM DOMAIN HERE"
 
 #commandthatexports-importsarticles
 #List all articles that need to be published
